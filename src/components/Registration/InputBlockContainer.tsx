d5a1d0fd92740d5eede3cc27c8694f200344524f
ValeriaPostylyakova <valeria.postylyakova@mail.ru>
2024-10-20 00:10:32 +0300

    сделана валидация формы, отправка полей в БД

---
+++ src/components/Registration/InputBlockContainer.tsx
@@ -0,0 +1,76 @@
+import * as React from 'react';
+import { FieldErrors, UseFormRegister } from 'react-hook-form';
+import { FormValues } from '../../page/Registration.tsx';
+
+type InputBlockContainerProps = {
+    register: UseFormRegister<FormValues>;
+    errors: FieldErrors<FormValues>;
+};
+
+const InputBlockContainer: React.FC<InputBlockContainerProps> = ({
+    register,
+    errors,
+}) => {
+    return (
+        <div className="registration__container-blocks">
+            <div className="registration__block">
+                <label>Имя</label>
+                <input
+                    {...register('firstName', {
+                        required: true,
+                        pattern: {
+                            value: /^[A-ZА-ЯЁ][a-zа-яё'’-]+$/i,
+                            message: 'Введите корректное имя',
+                        },
+                    })}
+                    placeholder="Введите ваше имя"
+                    type="text"
+                />
+                {errors?.firstName && (
+                    <p className="registration__error-message">
+                        {errors.firstName.message}
+                    </p>
+                )}
+            </div>
+            <div className="registration__block">
+                <label>Фамилия</label>
+                <input
+                    {...register('lastName', {
+                        pattern: {
+                            value: /^[A-ZА-ЯЁ][a-zа-яё'’-]+$/i,
+                            message: 'Введите корректную фамилию',
+                        },
+                    })}
+                    placeholder="Введите вашу фамилию (необязательно)"
+                    type="text"
+                />
+                {errors?.lastName && (
+                    <p className="registration__error-message">
+                        {errors.lastName.message}
+                    </p>
+                )}
+            </div>
+            <div className="registration__block">
+                <label>Почта</label>
+                <input
+                    {...register('email', {
+                        required: true,
+                        pattern: {
+                            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                            message: 'Введите корретный адрес',
+                        },
+                    })}
+                    placeholder="Введите вашу почту"
+                    type="text"
+                />
+                {errors?.email && (
+                    <p className="registration__error-message">
+                        {errors.email.message}
+                    </p>
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default InputBlockContainer;
--- src/page/Registration.tsx
+++ src/page/Registration.tsx
@@ -1,33 +1,47 @@
-import InputBlock from '../components/Registration/InputBlock.tsx';
-import { RiNeteaseCloudMusicLine } from 'react-icons/ri';
+import * as React from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
-const Registration = () => {
+import { RiNeteaseCloudMusicLine } from 'react-icons/ri';
+import InputBlockContainer from '../components/Registration/InputBlockContainer.tsx';
+
+export type FormValues = {
+    firstName: string;
+    lastName: string;
+    email: string;
+};
+
+const Registration: React.FC = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<FormValues>({
+        mode: 'onChange',
+    });
+
+    const navigate = useNavigate();
+    const onSubmit: SubmitHandler<FormValues> = async (data) => {
+        await axios.post('https://985cc4acb156d262.mokky.dev/users', data);
+        navigate('/');
+    };
     return (
         <div className="registration-wrapper">
-            <form className="registration">
+            <form onSubmit={handleSubmit(onSubmit)} className="registration">
                 <div className="registration__container">
                     <h1>Регистрация</h1>
-                    <div className="registration__container-blocks">
-                        <InputBlock label="Введите ваше имя" inputType="text" />
-                        <InputBlock
-                            label="Введите вашу фамилию"
-                            inputType="text"
-                        />
-                        <InputBlock
-                            label="Введите вашу почту"
-                            inputType="email"
-                        />
-                        <div className="registration__container-bottom">
-                            <div className="header__logo">
-                                <RiNeteaseCloudMusicLine className="registration__logo_icon" />
-                                <div>
-                                    <p>DEEP</p>
-                                    <p>SOUND</p>
-                                </div>
-                            </div>
-                            <button>Зарегистироваться</button>
+                    <InputBlockContainer register={register} errors={errors} />
+                </div>
+                <div className="registration__container-bottom">
+                    <div className="header__logo">
+                        <RiNeteaseCloudMusicLine className="registration__logo_icon" />
+                        <div>
+                            <p>DEEP</p>
+                            <p>SOUND</p>
                         </div>
                     </div>
+                    <button type="submit">Зарегистироваться</button>
                 </div>
             </form>
         </div>
